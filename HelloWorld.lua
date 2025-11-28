4309c770d72dde7ac61e81aa301e54edb1abcc80
Pongsathon <ksi.seam24@gmail.com>
2025-11-29 02:43:26 +0700

    Add Lua script for player management and settings

---
+++ HelloWorld.lua
@@ -0,0 +1,68 @@
+local Scripts = {}
+local Settings = {}
+
+Scripts.FindTextAndChange = function(title, text, IndexNumber)
+	for i, v in pairs(game.CoreGui:GetDescendants()) do
+		if v:IsA("TextLabel") and v.Text == "Disconnected" then
+			v.Text = title
+			return 'Gonna done'
+		elseif v:IsA("TextLabel") and string.find(v.Text, tostring(IndexNumber)) then
+			v.Text = text
+			return true
+		end
+	end
+	return nil
+end
+
+Scripts.Kick = function(Title , Reason)
+	local Randoms = math.random(1000,99000)
+	game.Players.LocalPlayer:Kick(Randoms)
+	repeat task.wait()
+		Scripts.FindTextAndChange(Title, Reason, Randoms)
+	until Scripts.FindTextAndChange(Title, Reason, Randoms) == true
+end
+
+Scripts.SC = function()
+	if not isfolder("YenixHubPremium") then
+		makefolder("YenixHubPremium")
+	end
+	if not isfolder("YenixHubPremium/" .. game.PlaceId) then
+		makefolder("YenixHubPremium/" .. game.PlaceId)
+	end
+
+	if isfile("YenixHubPremium/" .. game.PlaceId .. "/" .. game.Players.LocalPlayer.Name .. "-config.json") then
+		local success, result =
+			pcall(
+				function()
+					return game:GetService('HttpService'):JSONDecode(
+						readfile(("YenixHubPremium/" .. game.PlaceId .. "/" .. game.Players.LocalPlayer.Name .. "-config.json"))
+					)
+				end
+			)
+		if success and type(result) == "table" then
+			Settings = result
+		end
+	end
+
+	for k, v in pairs(Settings) do
+		_G[k] = v
+	end
+end
+
+Scripts.S = function()
+	return Settings
+end
+
+Scripts.SG = function()
+	for k, v in pairs(_G) do
+		if typeof(v) == "boolean" or typeof(v) == "string" or typeof(v) == "number" then
+			Settings[k] = v
+		end
+	end
+	writefile(
+		"YenixHubPremium/" .. game.PlaceId .. "/" .. game.Players.LocalPlayer.Name .. "-config.json",
+		game:GetService('HttpService'):JSONEncode(Settings)
+	)
+end
+
+return Scripts
